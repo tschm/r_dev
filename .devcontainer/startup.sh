c0514a9d6c52baba39f36f4a95711f1fa31507e1
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-07 18:57:38 +0400

    r container

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -5,8 +5,3 @@ apt update
 #add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 #curl -fsSL https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc
 #gpg --dearmor -o /usr/share/keyrings/r-archive-keyring.gpg
-
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2023.09.1-524-amd64.deb
-apt install ./rstudio-2023.09.1-524-amd64.deb
-
-rstudio
